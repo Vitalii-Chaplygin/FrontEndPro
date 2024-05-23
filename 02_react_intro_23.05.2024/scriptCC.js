b75038b127dc8fa3e97d2e4a04861c2c7c235248
Vitalii Chaplygin <chapliginvitaliy@gmail.com>
2024-05-23 20:43:09 +0200

    lesson2

---
+++ 02_react_intro_23.05.2024/scriptCC.js
@@ -0,0 +1,15 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+class Cohort40 extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hi Cohort40</h1>
+        <h1>Hi Cohort40</h1>
+      </>
+    );
+  }
+}
+root.render(
+  <Cohort40/>
+)
